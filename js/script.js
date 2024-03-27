c497a044cea0db8340df1811c610dc9268cb15f6
zakariya.elgallouchi <zakariya.elgallouchi@monlycee.net>
2024-03-27 08:31:45 +0100

    modified the style of the page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,48 @@
-const btn = document.querySelector('.btn');
+const btn = document.querySelector(".btn");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
 
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    })
+// Commentaires
+let message = document.querySelector("#message");
+let envoyer = document.querySelector("#envoyer");
+let messageList = document.querySelector("#message-list");
 
-})
+let output = "";
+envoyer.addEventListener("click", (e) => {
+  e.preventDefault();
+  let storage = localStorage.getItem("messages");
+  console.log(storage)
+  storage += `
+  <li>
+    <div class="header-comm">
+      <img src="./images/user.png" alt="" />
+      <span>Anonyme</span>
+    </div>
+    <p>${message?.value}</p>
+  </li>
+  `;
+  //   messageList.innerHTML = output;
+  JSON.stringify(localStorage.setItem("messages", storage));
+  messagesStorage();
+  message.value = "";
+});
 
+function messagesStorage() {
+  if (localStorage.getItem("messages") != null) {
+    console.log("existe");
+    let storage = localStorage.getItem("messages");
+    console.log(storage);
+    messageList.innerHTML += storage;
+  } else {
+    console.log("existe pas");
+
+    localStorage.setItem("messages", "");
+  }
+}
+messagesStorage();
